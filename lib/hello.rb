45cb54d1738e83cdb97543f4c1345b8aff85fcb3
Trevis McGill <trevismcgill25@gmail.com>
2020-07-29 19:11:32 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,13 @@
 def hello_t(array)
-  i = 0
+  if block_given?  
+    i = 0
 
   while i < array.length
     yield array[i]
     i = i + 1
   end
   array
+enclosedputs "Hey! No block was given!"
 end
 
 # call your method here!
